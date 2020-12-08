f0c65c02896025e16d16c87e2cd01851bcfa9bc8
woodelin-florveus <woodelinflorveus@gmail.com>
2020-12-08 14:29:26 -0500

    commit tree cabcdc8e12854531ab1f123e3c855196df233ff0

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
-# Make your shoe class here!
+class Shoe 
+
+    #getter method and how im creating methods
+    attr_accessor  :color, :size, :material, :condition
+    attr_reader :brand
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+
+end
